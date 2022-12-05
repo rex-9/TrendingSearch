d30ff7412ac52f1a4e1555d596b13d9ce6300eef
Htet Naing <htetnaing0814@gmail.com>
2022-12-05 22:41:02 +0630

    Add Integration tests on Home Page

--- spec/integration/trends_spec.rb
+++ spec/integration/trends_spec.rb
@@ -3,6 +3,12 @@ require 'rails_helper'
 RSpec.describe 'Trends', type: :system, js: true do
   describe 'index page' do
     before(:example) do
+      @trend = Trend.create(keyword: 'What is Ruby on Rails?')
+      @user = User.create(username: 'Rex', password: 'password')
+      visit user_session_path
+      fill_in 'Username', with: 'Rex'
+      fill_in 'Password', with: 'password'
+      click_button 'Log in'
       # visit new_user_registration_path
       # fill_in "Username",              :with => "Hello"
       # fill_in "Password",              :with => "password"
@@ -10,11 +16,11 @@ RSpec.describe 'Trends', type: :system, js: true do
 
       # click_button "Sign up"
 
-      @user = User.create(username: 'Rex', password: 'password')
-      sign_in @user
+      # @user = User.create(username: 'Rex', password: 'password')
+      # sign_in @user
 
-      @trend = Trend.create(keyword: 'What is Ruby on Rails?')
-      visit root_path
+      # @trend = Trend.create(keyword: 'What is Ruby on Rails?')
+      # visit root_path
     end
 
     it 'renders Name of the Webpage' do
@@ -26,7 +32,7 @@ RSpec.describe 'Trends', type: :system, js: true do
     end
 
     it 'renders Icon of the Magnifying glass' do
-      expect(page).to have_selector('img[alt="lens"]')
+      expect(page).to have_css("#lens")
     end
 
     it 'renders Name of the Trend searched by the User' do
@@ -37,9 +43,9 @@ RSpec.describe 'Trends', type: :system, js: true do
       expect(page).to have_content('1')
     end
 
-    it 'redirects to Purchases path when a category is clicked' do
-      click_button @trend.keyword
-      expect(page).to have_current_path(trend_path(@trend.id))
+    it 'redirects to Trend path when a keyword is clicked' do
+      click_on @trend.keyword
+      expect(page).to have_current_path(trend_path(1))
     end
   end
 end
