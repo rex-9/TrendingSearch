89b804b0fcfe0e9aef88d0b0a53d41da531935d1
Htet Naing <htetnaing0814@gmail.com>
2022-12-06 13:35:18 +0630

    Complete Admin Feature

--- config/routes.rb
+++ config/routes.rb
@@ -4,8 +4,8 @@ Rails.application.routes.draw do
   resources :trends, only: [:show, :create, :destroy]
 
   get "/dashboard", to: "admin#dashboard"
-  get "/users/:id/trends", to: "admin#user_trends"
-  get "/trends/:id/users", to: "admin#trend_users"
+  get "/users/:id/trends", to: "admin#user_trends", as: "user_trends"
+  get "/trends/:id/users", to: "admin#trend_users", as: "trend_users"
 
   # Defines the root path route ("/")
   root to: "trends#search"
